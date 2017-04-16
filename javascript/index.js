b9dcd4587da8589732c1ef9f96accea5302c87ba
Sosoyy <610204448@qq.com>
2017-04-16 16:06:47 +0800

    no message

--- javascript/index.js
+++ javascript/index.js
@@ -15,45 +15,13 @@
 
 var deleteLog = false;
 
+
 $(document).ready(function () {
     $('#pagepiling').pagepiling({
         menu: '#menu',
-        anchors: ['intro', 'aa', 'bb','cc','dd'],
+        anchors: ['intro', 'aa', 'bb', 'cc', 'dd'],
         sectionsColor: ['#292929', '#292929', '#292929', '#292929'],
-        onLeave: function (index, nextIndex, direction) {
-            if (deleteLog) {
-                $('#callbacksDiv').html('');
-            }
-        },
-        afterRender: function () {
-            $('#callbacksDiv').append('<p>afterRender</p>');
-        },
-        afterLoad: function (anchorLink, index) {
-            $('#callbacksDiv').append('<p>afterLoad - anchorLink:' + anchorLink + " index:" + index + '</p>');
-
-            //section 2
-            if (index == 2) {
-                //moving the image
-                $('#section2').find('.intro').delay(100).animate({
-                    left: '0%'
-                }, 1500, 'easeOutExpo', function () {
-                    $('#section2').find('p').first().fadeIn(700, function () {
-                        $('#section2').find('p').last().fadeIn(600);
-                    });
-                });
-
-
-            }
-
-            //section 3
-            if (anchorLink == 'page3') {
-                //section 3
-                $('#section3').find('.intro').delay(100).animate({
-                    left: '0%'
-                }, 1500, 'easeOutExpo');
-            }
-
-            deleteLog = true;
-        }
+        loopTop: true,
+        loopBottom: true
     });
 });

06674a8135660638e230e8718953ed9ad583b154
Sosoyy <610204448@qq.com>
2017-04-16 12:54:26 +0800

    no message

--- javascript/index.js
+++ javascript/index.js
@@ -8,7 +8,7 @@
 
     function WindowResize() {
         deviceWidth = document.documentElement.clientWidth;
-        if (deviceWidth > 750) deviceWidth = 750;
+        if (deviceWidth > 500) deviceWidth = 500;
         document.documentElement.style.fontSize = deviceWidth / 7.5 + 'px';
     }
 })(window);
